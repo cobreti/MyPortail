44b8dc78d39b8f21cc9684de827eae24835b4604
dannyt <cobreti_@hotmail.com>
2018-06-09 15:56:13 -0400

    added base for authService

---
+++ auth/authService/authService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace authService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var configuration = new ConfigurationBuilder()
+              .AddJsonFile("hosting.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"hosting.{envName}.json", optional: true, reloadOnChange: true)
+              .AddCommandLine(args)
+              .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(configuration)
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
